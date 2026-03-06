731ddd0ea705dced7822cdc60c74005f02c0f9c9
Nguyễn Anh Tân <joseph.nguyenanhtan@outlook.com>
2026-03-06 15:30:41 +0700

    adg

--- routes/root.js
+++ routes/root.js
@@ -1484,6 +1484,59 @@ module.exports = async function (fastify, opts) {
   });
 
 }
+fastify.get('/get-quick-utilities-v2', async function (request, reply) {
+    // const tb_quick_utilities = this.mongo.db.collection('quick-utilities')
+    // let quick_utilities =  await tb_quick_utilities.find({}).toArray()
+    let quick_utilities = 
+    {
+      "buttonColors" : {
+        "cgkpv": "#c0392b",
+        "reading": "#8e44ad",
+        "lich": "#16a085",
+        "homNay": "#2980b9",
+        "homNayDisable": "#95a5a6",
+      },
+    "data":[
+      {
+          id: 'retreat-confession-schedule',
+          title: 'Lịch tĩnh tâm/giải tội',
+          icon: 'calendar-outline',
+          url: 'https://www.tonggiaophanhanoi.org/lich-tinh-tam-mua-chay-va-tuan-thanh-2026-tai-cac-nha-tho-noi-thanh-ha-noi/',
+      },
+      {
+          id: 'reflection',
+          title: 'Suy tư tản mạn',
+          icon: 'bulb-outline',
+          url: 'https://www.tonggiaophanhanoi.org/category/song-dao/suy-tu-tan-man/',
+      },
+      {
+          id: 'catechism',
+          title: 'Giáo lý', 
+          icon: 'school-outline',
+          url: 'https://www.tonggiaophanhanoi.org/category/song-dao/giao-ly-cho-nguoi-truong-thanh/',
+      },
+      {
+          id: 'saints',
+          title: 'Hạnh các thánh',
+          icon: 'sparkles-outline',
+          url: 'https://www.tonggiaophanhanoi.org/category/phung-vu/hanh-cac-thanh/',
+      },
+      {
+          id: 'historical-documents',
+          title: 'Tài liệu lịch sử',
+          icon: 'book-outline',
+          url: 'https://www.tonggiaophanhanoi.org/tu-lieu-lich-su/',
+      },
+      
+    ]
+  }
+    try{
+      return quick_utilities
+    }catch(err){
+      return err
+    }
+  });
+
 
 function removeVietnameseTones(str) {
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
