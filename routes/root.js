f03d6801a8a0923d49e9c0ace22168d89f5a9c54
Nguyễn Anh Tân <joseph.nguyenanhtan@outlook.com>
2026-03-13 10:00:10 +0700

    gf

--- routes/root.js
+++ routes/root.js
@@ -1538,6 +1538,18 @@ fastify.get('/get-quick-utilities-v2', async function (request, reply) {
           url: 'https://www.tonggiaophanhanoi.org/category/phung-vu/hanh-cac-thanh/',
       },
       {
+          id: 'general-prayers',
+          title: 'Lời nguyện tín hữu',
+          icon: 'prayer-outline',
+          url: 'https://www.tonggiaophanhanoi.org/loi-nguyen-tin-huu-2/',
+      }, 
+      {
+          id: 'synod-documents',
+          title: 'Văn kiện hậu công nghị',
+          icon: 'scroll-outline',
+          url: 'https://www.tonggiaophanhanoi.org/van-kien-hau-cong-nghi/',
+      }, 
+      {
           id: 'historical-documents',
           title: 'Tư liệu lịch sử',
           icon: 'book-outline',
